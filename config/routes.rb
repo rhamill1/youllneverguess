3daebebce4354fc06c07505f514532a687d7b2fc
rhamill1 <rhamill11@gmail.com>
2017-09-04 17:48:47 -0400

    cleans up routes formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
-
-  get 'static_pages/submission_success'
-
-  get 'static_pages/faq'
-
   root to: 'static_pages#home'
   get '/', to: 'static_pages#home'
 
   resources :ideas
+
+  get 'static_pages/home'
+  get 'static_pages/submission_success'
+  get 'static_pages/faq'
 
 end
