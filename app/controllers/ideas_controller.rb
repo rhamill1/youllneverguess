953b3213d6fed42065313ef845dd9ca38febcaa1
rhamill1 <rhamill11@gmail.com>
2017-10-16 20:59:10 -0400

    sets up captcha

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,16 +1,22 @@
 class IdeasController < ApplicationController
+  include SimpleCaptcha::ControllerHelpers
 
   def new
      @idea = Idea.new
   end
 
   def create
-    @idea = Idea.create(idea_params)
-    if @idea.save
-      flash[:success] = 'Idea Created!'
-      redirect_to root_path
+    if simple_captcha_valid?
+      @idea = Idea.create(idea_params)
+      if @idea.save
+        flash[:success] = 'Idea Created!'
+        redirect_to root_path
+      else
+        flash[:error] = @idea.errors.full_messages.join(' ')
+      end
     else
-      flash[:error] = @idea.errors.full_messages.join(' ')
+      redirect_to root_path
+      flash[:notice] = "captcha did not match"
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170902224639) do
+ActiveRecord::Schema.define(version: 20170905222834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170902224639) do
     t.string   "author"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "simple_captcha_data", force: :cascade do |t|
+    t.string   "key",        limit: 40
+    t.string   "value",      limit: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["key"], name: "idx_key", using: :btree
   end
 
 end
