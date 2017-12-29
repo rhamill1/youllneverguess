c232d2ae48925de462a511a133b670db0fe476cc
rhamill1 <rhamill11@gmail.com>
2017-12-29 12:17:17 -0500

    updates iframe dns bug 2

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,9 @@ module YoullNeverGuess
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
 
-config.action_dispatch.default_headers = {
-  'X-Frame-Options' => 'ALLOWALL'
-}
